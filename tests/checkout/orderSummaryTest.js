f4d80e063fd6890016e29522f4a87b2a02c58000
yfu <toainth@gmail.com>
2024-06-25 11:44:59 +0700

    Create more tests case

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -43,6 +43,22 @@ describe('Test Suite: renderOrderSummary', () => {
         expect(
             document.querySelector(`.js-product-quantity-${productId2}`).innerText
         ).toContain('Quantity: 1');
+
+        expect(
+            document.querySelector(`.js-product-name-${productId1}`).innerText
+        ).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+
+        expect(
+            document.querySelector(`.js-product-name-${productId2}`).innerText
+        ).toEqual('Intermediate Size Basketball');
+
+        expect(
+            document.querySelector(`.js-product-price-${productId1}`).innerText
+        ).toEqual('$10.90');
+
+        expect(
+            document.querySelector(`.js-product-price-${productId2}`).innerText
+        ).toEqual('$20.95');
     });
 
     it('removes a product', () => {
@@ -60,6 +76,14 @@ describe('Test Suite: renderOrderSummary', () => {
         expect(
             document.querySelector(`.js-cart-item-container-${productId2}`)
         ).not.toEqual(null);
+
+        expect(
+            document.querySelector(`.js-product-name-${productId2}`).innerText
+        ).toEqual('Intermediate Size Basketball');
+
+        expect(
+            document.querySelector(`.js-product-price-${productId2}`).innerText
+        ).toEqual('$20.95');
 
         expect(cart.length).toEqual(1);
         expect(cart[0].productId).toEqual(productId2);
