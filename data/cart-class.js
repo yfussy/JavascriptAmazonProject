4880fa0eba5d72b3a9e08968f17f4f6c4a1feb80
yfu <toainth@gmail.com>
2024-06-25 20:34:23 +0700

    Remove console. log(cart)

--- data/cart-class.js
+++ data/cart-class.js
@@ -109,8 +109,4 @@ class Cart {
 
 
 export const cart = new Cart('cart-oop');
-const businessCart = new Cart('cart-business');
 
-console.log(cart);
-console.log(businessCart);
-console.log(businessCart instanceof Cart);
