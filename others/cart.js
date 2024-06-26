22d1ab5b52c238723e7218ee78d7ae35d4f01108
yfu <toainth@gmail.com>
2024-06-26 15:31:33 +0700

    Clean up code

--- others/cart.js
+++ others/cart.js
@@ -1,4 +1,4 @@
-import { validDeliveryOption } from "./deliveryOptions.js";
+import { validDeliveryOption } from "../data/deliveryOptions.js";
 
 export let cart;
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,14 +2,11 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProductsFetch } from "../data/products.js";
-import { loadCartFetch } from "../data/cart.js";
+import { loadCartFetch } from "../others/cart.js";
 
 async function loadPage() {
     try {
-        await Promise.all([
-            loadProductsFetch(),
-            loadCartFetch()
-        ]);
+        await loadProductsFetch();
 
     } catch (error) {
         console.log('Unexpected error. Please try again later.');
