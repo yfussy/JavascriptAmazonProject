31bf67283f3c43c281cb0799595f3b8c55aa0638
yfu <toainth@gmail.com>
2024-06-26 12:31:21 +0700

    Generate orderList HTML

---
+++ scripts/orders.js
@@ -0,0 +1,106 @@
+import { getProduct, loadProductsFetch, products } from "../data/products.js"
+import { orders } from "../data/orders.js";
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import formatCurrency from "./utils/money.js";
+import { cart } from "../data/cart-class.js"
+
+async function loadPage() {
+    await loadProductsFetch();
+
+    let ordersHTML = '';
+
+
+    orders.forEach(order => {
+        console.log(order);
+        const orderTimeString = dayjs(order.orderTime).format('MMMM D');
+
+        ordersHTML += `
+            <div class="order-container">
+          
+                <div class="order-header">
+                    <div class="order-header-left-section">
+                    <div class="order-date">
+                        <div class="order-header-label">Order Placed:</div>
+                        <div>${orderTimeString}</div>
+                    </div>
+                    <div class="order-total">
+                        <div class="order-header-label">Total:</div>
+                        <div>$${formatCurrency(order.totalCostCents)}</div>
+                    </div>
+                    </div>
+
+                    <div class="order-header-right-section">
+                    <div class="order-header-label">Order ID:</div>
+                    <div>${order.id}</div>
+                    </div>
+                </div>
+
+                <div class="order-details-grid">
+                    ${productListHTML(order)}
+                </div>
+            </div>    
+        `;
+
+        function productListHTML(order) {
+            let productListHTML = '';
+
+            order.products.forEach((productDetails) => {
+                const product = getProduct(productDetails.productId);
+                const deliveryTimeString = dayjs(productDetails.estimatedDeliveryTime).format('MMMM D');
+
+                productListHTML += `
+                    <div class="product-image-container">
+                        <img src="${product.image}">
+                    </div>
+
+                    <div class="product-details">
+                        <div class="product-name">
+                            ${product.name}
+                        </div>
+                        <div class="product-delivery-date">
+                            Arriving on: ${deliveryTimeString}
+                        </div>
+                        <div class="product-quantity">
+                            Quantity: ${productDetails.quantity}
+                        </div>
+                        <button class="buy-again-button js-buy-again-button button-primary"
+                        data-product-id="${product.id}">
+                            <img class="buy-again-icon" src="images/icons/buy-again.png">
+                            <span class="buy-again-message">Buy it again</span>
+                        </button>
+                    </div>
+
+                    <div class="product-actions">
+                        <a href="tracking.html?orderId=${order.id}&productId=${product.id}">
+                            <button class="track-package-button button-secondary">
+                            Track package
+                            </button>
+                        </a>
+                    </div>
+                `;
+            });
+
+            return productListHTML;
+        }
+
+        document.querySelector('.js-orders-grid').innerHTML = ordersHTML;
+
+        document.querySelectorAll('.js-buy-again-button')
+            .forEach(button => {
+                button.addEventListener('click', () => {
+                    cart.addtoCart(button.dataset.productId);
+
+                    button.innerHTML = 'Added';
+                    setTimeout(() => {
+                        button.innerHTML = `
+                            <img class="buy-again-icon" 
+                            src="images/icons/buy-again.png">
+                            <span class="buy-again-message">Buy it again</span>
+                        `;
+                    }, 1000);
+                });
+            });
+    });
+}
+
+loadPage();
